9c0e5fe46468b1114fa62fb4b0a2a2cc92697899
Adalia Lee <adalialee@gmail.com>
2024-04-04 18:02:03 -0400

    updated image sizing

--- src/Portfolio/ProductDesign/index.tsx
+++ src/Portfolio/ProductDesign/index.tsx
@@ -46,7 +46,7 @@ function ProductDesign() {
                                 </div>
                             </div>
                             <div style={{ display: 'flex' }}>
-                                <div style={{ width: '50vw', marginLeft: "24px" }}>
+                                <div style={{ width: '38vw', marginLeft: "5vw" }}>
                                     <img src={experience.image} className="img-fluid" style={{marginBottom: '5vw'}}/>
                                 </div>
                             </div>
